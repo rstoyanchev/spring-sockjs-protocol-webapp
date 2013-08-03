b8e0cfb042c1def56161ca08de455ee67609527f
Rossen Stoyanchev <rstoyanchev@gopivotal.com>
2013-08-03 11:23:00 -0400

    Add exception resolver
    Also enable dummy session cookie required for SockJS tests and which
    is now turned off by default.

--- src/main/java/org/springframework/samples/sockjs/WebConfig.java
+++ src/main/java/org/springframework/samples/sockjs/WebConfig.java
@@ -3,10 +3,17 @@ package org.springframework.samples.sockjs;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.servlet.mvc.support.DefaultHandlerExceptionResolver;
+import org.springframework.web.socket.sockjs.SockJsException;
 import org.springframework.web.socket.sockjs.SockJsHttpRequestHandler;
 import org.springframework.web.socket.sockjs.transport.handler.DefaultSockJsService;
 
@@ -20,6 +27,7 @@ public class WebConfig {
 		DefaultSockJsService mainService = new DefaultSockJsService(sockJsTaskScheduler());
 		mainService.setValidSockJsPrefixes("/echo", "/close", "/cookie_needed_echo");
 		mainService.setStreamBytesLimit(4096);
+		mainService.setDummySessionCookieEnabled(true);
 
 		DefaultSockJsService anotherService = new DefaultSockJsService(sockJsTaskScheduler());
 		anotherService.setValidSockJsPrefixes("/disabled_websocket_echo");
@@ -38,10 +46,27 @@ public class WebConfig {
 	}
 
 	@Bean
+	public HandlerExceptionResolver exceptionResolver() {
+		return new SockJsExceptionResolver();
+	}
+
+	@Bean
 	public ThreadPoolTaskScheduler sockJsTaskScheduler() {
 		ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
 		taskScheduler.setThreadNamePrefix("SockJS-");
 		return taskScheduler;
+	}
+
+
+	private static class SockJsExceptionResolver extends DefaultHandlerExceptionResolver {
+
+		public ModelAndView doResolveException(HttpServletRequest rq, HttpServletResponse rs, Object h, Exception e) {
+			if (e instanceof SockJsException) {
+				rs.setStatus(500);
+				return new ModelAndView();
+			}
+			return super.doResolveException(rq, rs, h, e);
+		}
 	}
 
 }
